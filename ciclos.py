7ee827c25cef6964d6d23f55c283eef02a731ed7
David Fernando Ortega Tamayo <df.ortegatamayo@ugto.mx>
2021-02-09 15:50:53 -0600

    Create condicionales.py

---
+++ ciclos.py
@@ -0,0 +1,24 @@
+
+continuar = True
+contador =0
+
+# while continuar:
+#     contador +=1
+#     print(f'{contador}')
+#
+#     print('Deseas continuar:')
+#     respuesta = input()
+#
+#     if respuesta == 's':
+#         continue
+#     else:
+#         continuar = False
+
+
+for i in range(1, 10, 2):
+    print(f'Numero:{i+1}')
+
+    if i == 5:
+        break
+
+print('FIN')
---
+++ condicionales.py
@@ -0,0 +1,12 @@
+
+verdadero= 5>3
+falso = 3>5
+
+if falso:
+    print('Hola')
+
+elif 4>6:
+    print('4>6')
+
+else:
+    print('Adios')
