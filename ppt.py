d16575ac2c268b864f87960dc265dad727e878d3
David Fernando Ortega Tamayo <df.ortegatamayo@ugto.mx>
2021-02-09 17:04:28 -0600

    Create ppt.py

---
+++ ppt.py
@@ -0,0 +1,17 @@
+from random import randint
+
+numero = randint(0,2)
+
+opciones = ['piedra', 'papel', 'tijera']
+
+#seleccion= int(input())
+seleccion =3
+
+#print(opciones[seleccion - 1])
+
+
+seleccion_pc = opciones[numero -1]
+
+print(seleccion_pc)
+
+
